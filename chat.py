a36a3b74bc6944871662c72e55eb208277b97a5c
Kdnn-S <techsharp.business@gmail.com>
2026-09-01 22:08:29 -0400

    simple comments

--- chat.py
+++ chat.py
@@ -1,9 +1,10 @@
-from openai import OpenAI
+from openai import OpenAI 
+# requires an OpenAI key lol
 
 client = OpenAI()
 
-user_prompt=("Prompt: ")
-system_prompt = "Limit your answer to one sentence. Pretend you're a cat."
+user_prompt=("Prompt: ") # where the user can input questions and stuff for the ai
+system_prompt = "Limit your answer to one sentence. Pretend you're a cat." # instructions for the ai to do
 
 response = client.responses.create(
     input=user_prompt,
